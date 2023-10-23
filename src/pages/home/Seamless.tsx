fefc6dad71a9933078553c641c622c2d589dd02f
jdavyds <slimjdavids@gmail.com>
2023-10-23 10:20:13 +0100

    update

--- src/pages/home/Seamless.tsx
+++ src/pages/home/Seamless.tsx
@@ -5,7 +5,7 @@ import phone from "@/assets/phone.png";
 
 export default function Seemless() {
   return (
-    <div className="bg-primary shadow-[0px_0px_52px_21px_rgba(203,31,82,0.20)] rounded-2xl mx-5 md:mx-20 md:h-[85vh] pt-14 md:pt-0 px-5 md:px-20 my-10 md:my-20 flex flex-col md:grid grid-cols-[1.5fr_2fr] grid-rows-1 gap-10 items-center">
+    <div className="bg-primary shadow-[0px_0px_52px_21px_rgba(203,31,82,0.20)] rounded-3xl mx-5 md:mx-20 md:h-[85vh] pt-14 md:pt-0 px-5 md:px-20 my-10 md:my-20 flex flex-col md:grid grid-cols-[1.5fr_2fr] grid-rows-1 gap-10 items-center">
       <div className="flex flex-col gap-10 md:gap-20 text-white">
         <h3 className="text-[36px] font-[700]">
           Seamless Services for Your Every Need: Hitch, Deliver, Excel.
@@ -15,14 +15,14 @@ export default function Seemless() {
           <div className="flex flex-col md:flex-row gap-5">
             <button className="bg-white flex flex-col py-3 px-8 gap-1 rounded-xl justify-center items-center w-fit min-h-fit">
               <small className="text-xs text-[#101010]">Download on</small>
-              <span className="flex gap-1 text-[#101010] font-[600]">
+              <span className="flex items-center gap-2 text-[#101010] font-[600]">
                 <Image src={playstore} alt="" className="" />
                 Play Store
               </span>
             </button>
             <button className="bg-white flex flex-col py-3 px-8 gap-1 rounded-xl justify-center items-center w-fit min-h-fit">
               <small className="text-xs text-[#101010]">Download on</small>
-              <span className="flex gap-1 text-[#101010] font-[600]">
+              <span className="flex items-center gap-2 text-[#101010] font-[600]">
                 <Image src={appstore} alt="" className="" />
                 App Store
               </span>
