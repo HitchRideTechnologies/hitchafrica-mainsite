c9e9c0c6cec4f27dc7a8b05895e04173ca080550
jdavyds <slimjdavids@gmail.com>
2023-10-25 00:37:08 +0100

    update

--- src/pages/terms/Terms.jsx
+++ src/pages/terms/Terms.jsx
@@ -1,125 +1,105 @@
-
 export default function Policies() {
   return (
-    <main
-    className="flex flex-col gap-10 md:gap-20 text-lg md:text-2xl  px-5 md:px-20 py-10 md:py-20 leading-loose"
-    >
-        <div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-1. Acceptance of Terms
-</h4>
-<p>
-
-By accessing or using [Your Company Name] services, you agree to comply with and be bound by these Terms and Conditions. If you do not agree with these terms, please refrain from using our services.
-</p>
-</div>
-        
-        <div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-2. Use of Services
-</h4>
-<p>
-
-You must be of legal age to use our services. You agree to use our services for lawful purposes and in accordance with all applicable laws and regulations.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-3. User Accounts
-</h4>
-<p>
-
-To access certain features of our services, you may be required to create a user account. You are responsible for maintaining the confidentiality of your account information and agree to notify us of any unauthorized use.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-4. Intellectual Property
-</h4>
-<p>
-
-All content, logos, trademarks, and intellectual property on our services are owned by [Your Company Name]. You agree not to reproduce, distribute, or create derivative works without our express consent.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-5. Prohibited Activities
-</h4>
-<ul className="list disc px-5">
-<li>
-
-You agree not to engage in any activities that may:
-</li>
-<li>
-Violate laws or regulations.
-</li>
-<li>
-Infringe on the rights of others.
-</li>
-<li>
-Interfere with the operation of our services.
-</li>
-</ul>
-</div>
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-6. Disclaimer of Warranties
-</h4>
-<p>
-
-Our services are provided "as is" without warranties of any kind, whether express or implied. We do not guarantee the accuracy, completeness, or reliability of our services.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-7. Limitation of Liability
-</h4>
-<p>
-
-To the fullest extent permitted by law, [Your Company Name] shall not be liable for any indirect, incidental, special, consequential, or punitive damages.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-8. Governing Law
-</h4>
-<p>
-
-These Terms and Conditions are governed by and construed in accordance with the laws of [Your Jurisdiction].
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-9. Changes to Terms
-</h4>
-<p>
-
-We reserve the right to modify or replace these Terms and Conditions at any time. Any changes will be effective immediately upon posting.
-</p>
-</div>
-
-
-<div className="flex flex-col gap-4">
-<h4 className="text-red font-[500] text-3xl md:text-[40px]">
-10. Contact Us
-</h4>
-<p>
-
-If you have any questions or concerns about these Terms and Conditions, please contact us at info@hitchafrica.com.
-</p>
-</div>
+    <main className="flex flex-col gap-10 md:gap-20 text-lg md:text-2xl  px-5 md:px-20 py-10 md:py-20 leading-loose">
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          1. Acceptance of Terms
+        </h4>
+        <p>
+          By accessing or using [Your Company Name] services, you agree to
+          comply with and be bound by these Terms and Conditions. If you do not
+          agree with these terms, please refrain from using our services.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          2. Use of Services
+        </h4>
+        <p>
+          You must be of legal age to use our services. You agree to use our
+          services for lawful purposes and in accordance with all applicable
+          laws and regulations.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          3. User Accounts
+        </h4>
+        <p>
+          To access certain features of our services, you may be required to
+          create a user account. You are responsible for maintaining the
+          confidentiality of your account information and agree to notify us of
+          any unauthorized use.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          4. Intellectual Property
+        </h4>
+        <p>
+          All content, logos, trademarks, and intellectual property on our
+          services are owned by [Your Company Name]. You agree not to reproduce,
+          distribute, or create derivative works without our express consent.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          5. Prohibited Activities
+        </h4>
+        <ul className="list disc px-5">
+          <li>You agree not to engage in any activities that may:</li>
+          <li>Violate laws or regulations.</li>
+          <li>Infringe on the rights of others.</li>
+          <li>Interfere with the operation of our services.</li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          6. Disclaimer of Warranties
+        </h4>
+        <p>
+          Our services are provided {`"as is"`} without warranties of any kind,
+          whether express or implied. We do not guarantee the accuracy,
+          completeness, or reliability of our services.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          7. Limitation of Liability
+        </h4>
+        <p>
+          To the fullest extent permitted by law, [Your Company Name] shall not
+          be liable for any indirect, incidental, special, consequential, or
+          punitive damages.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          8. Governing Law
+        </h4>
+        <p>
+          These Terms and Conditions are governed by and construed in accordance
+          with the laws of [Your Jurisdiction].
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          9. Changes to Terms
+        </h4>
+        <p>
+          We reserve the right to modify or replace these Terms and Conditions
+          at any time. Any changes will be effective immediately upon posting.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h4 className="text-red font-[500] text-3xl md:text-[40px]">
+          10. Contact Us
+        </h4>
+        <p>
+          If you have any questions or concerns about these Terms and
+          Conditions, please contact us at info@hitchafrica.com.
+        </p>
+      </div>
     </main>
   );
 }
