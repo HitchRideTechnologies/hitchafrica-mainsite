da9f48ceed28fb3bbd6fbfe719fefb8737d0af3d
jdavyds <slimjdavids@gmail.com>
2023-10-25 13:21:05 +0100

    update

--- src/pages/home/Others.tsx
+++ src/pages/home/Others.tsx
@@ -24,16 +24,16 @@ export default function Others() {
           className="h-auto md:h-[90vh] flex flex-col justify-evenly gap-10 py-20 pl-5 md:pl-10 bg-[rgba(4,187,198,0.20)] relative otherbg bg-contain"
           style={{ backgroundImage: `url(${other1.src})` }}
         >
-          <div className="w-3/5 flex flex-col gap-5">
+          <div className="md:min-w-fit md:w-fit flex flex-col gap-5">
             <span className="other1 font-[600] text-xl">Hitch Ride</span>
             <div className="flex flex-col items-start gap-5">
               <h3 className="text-3xl md:text-[46px] font-[600] leading-snug">
-                Book. Ride. Arrive. Enjoy.
+                Book. Ride. <br className="hidden md:block" /> Arrive. Enjoy.
               </h3>
             </div>
             <p className="text-xl md:text-2xl">
-              Book a ride, send and receive a package effortlessly. Experience
-              the journey joyfully.
+              Book a ride, send and receive <br className="hidden md:block" /> a package effortlessly. <br className="hidden md:block" /> Experience
+              the journey <br className="hidden md:block" /> joyfully.
             </p>
           </div>
           <button className="flex items-center gap-3 text-lg">
@@ -44,15 +44,15 @@ export default function Others() {
           className="h-auto md:h-[90vh] flex flex-col justify-evenly gap-10 py-20 pl-5 md:pl-10 bg-[rgba(153,113,195,0.10)] relative otherbg bg-contain"
           style={{ backgroundImage: `url(${other2.src})` }}
         >
-          <div className="w-3/5 flex flex-col gap-5">
+          <div className="md:min-w-fit md:w-fit flex flex-col gap-5">
             <span className="other2 font-[600] text-xl">Hitch Food</span>
             <div className="flex flex-col items-start gap-5">
               <h3 className="text-3xl md:text-[46px] font-[600] leading-snug">
-                Order, Delight. Eat.
+                Order, Delight. <br className="hidden md:block" /> Eat.
               </h3>
             </div>
             <p className="text-xl md:text-2xl">
-              Savor a feast of flavors with our swift food ordering and delivery
+              Savor a feast of flavors <br className="hidden md:block" /> with our swift food <br className="hidden md:block" /> ordering and delivery <br className="hidden md:block" />
               service.
             </p>
           </div>
@@ -64,18 +64,17 @@ export default function Others() {
           className="h-auto md:h-[90vh] flex flex-col justify-evenly gap-10 py-20 pl-5 md:pl-10 bg-[#E7F2D6] relative otherbg bg-contain"
           style={{ backgroundImage: `url(${other3.src})` }}
         >
-          <div className="w-3/5 flex flex-col gap-5">
+          <div className="md:min-w-fit md:w-fit flex flex-col gap-5">
             <span className="other3 font-[600] text-xl">
               Hitch Shopping Mart
             </span>
             <div className="flex flex-col items-start gap-5">
               <h3 className="text-3xl md:text-[46px] font-[600] leading-snug">
-                Discover. Order. Receive. Enjoy.
+                Discover. Order. <br className="hidden md:block" /> Receive. Enjoy.
               </h3>
             </div>
             <p className="text-xl md:text-2xl">
-              Shopping for all your products an groceries with ease and have it
-              delivered to your doorsteps
+            On-Demand Shopping for <br className="hidden md:block" /> all your products and <br className="hidden md:block" /> groceries with ease and <br className="hidden md:block" /> have it delivered to your <br className="hidden md:block" /> doorsteps
             </p>
           </div>
           <button className="flex items-center gap-3 text-lg">
@@ -86,17 +85,17 @@ export default function Others() {
           className="h-auto md:h-[90vh] flex flex-col justify-evenly gap-10 py-20 pl-5 md:pl-10 bg-[rgba(242,71,137,0.10)] relative otherbg bg-contain"
           style={{ backgroundImage: `url(${other4.src})` }}
         >
-          <div className="w-3/5 flex flex-col gap-5">
+          <div className="md:min-w-fit md:w-fit flex flex-col gap-5">
             <span className="other4 font-[600] text-xl">
               Hitch Professional Services
             </span>
             <div className="flex flex-col items-start gap-5">
               <h3 className="text-3xl md:text-[46px] font-[600] leading-snug">
-                Find the Best Talents with Ease
+                Find the Best <br className="hidden md:block" /> Talents with Ease
               </h3>
             </div>
             <p className="text-xl md:text-2xl">
-              The best talents local to you or remote to complete assigned tasks
+              The best talents local to you <br className="hidden md:block" /> or remote to complete <br className="hidden md:block" /> assigned tasks
             </p>
           </div>
           <button className="flex items-center gap-3 text-lg">
